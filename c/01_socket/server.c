521c8bcb36c6887edf744200cc03714208b26957
atfuture7 <to.future7@gmail.com>
2020-05-07 14:11:34 -0700

    code for client side

--- c/01_socket/server.c
+++ c/01_socket/server.c
@@ -30,7 +30,7 @@ int main(int argc, char *argv[]) {
     if (sockfd < 0) {
         error("ERROR opening socket");
     }
-    bzero( (void *)&serv_addr, sizeof(serv_addr));
+    bzero( (char *)&serv_addr, sizeof(serv_addr));
     portno = atoi(argv[1]);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = INADDR_ANY;
