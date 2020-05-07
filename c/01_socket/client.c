913277430be980aafac7ad3602b0c6a5b2215a19
atfuture7 <to.future7@gmail.com>
2020-05-07 21:30:25 +0000

    bug fixed for client

--- c/01_socket/client.c
+++ c/01_socket/client.c
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <errno.h>
 #include <unistd.h>
+#include <netdb.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
@@ -28,12 +29,12 @@ int main(int argc, char *argv[]) {
     }
 
     portno = atoi(argv[2]);
-    sockfd = socket(AF_NET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd <0 ) error("ERROR opening socket");
 
     server = gethostbyname(argv[1]);
     if (server == NULL) {
-        fprintf(stderr, "ERROR, no such host")
+        fprintf(stderr, "ERROR, no such host");
         exit(0);
     }
 
@@ -41,7 +42,7 @@ int main(int argc, char *argv[]) {
     serv_addr.sin_family = AF_INET;
     bcopy((char *) server->h_addr,
             (char *) &serv_addr.sin_addr.s_addr,
-            serv_addr->h_length);
+            server->h_length);
     serv_addr.sin_port = htons(portno);
     if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) <0 )
         error("ERROR connecting");
